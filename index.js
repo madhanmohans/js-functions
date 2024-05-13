61cb80694e1359678cfaba4de0fbed49abf93c18
madhanmohans <sherlockholmes.missme@gmail.com>
2024-05-13 12:03:33 +0530

    implememted custom filter

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/*
+The map() function is used to transform each element 
+of an array by applying a provided function to it. 
+It creates a new array with the transformed elements, 
+while leaving the original array unchanged.
+*/
+
 function customMap(arr, callbackFn) {
     const newArr = [];
     for (let i = 0; i < arr.length; i++) {
@@ -9,3 +16,20 @@ function customMap(arr, callbackFn) {
 const numbers = [5, 4, 3, 2, 1];
 const doubledNumbers = customMap(numbers, (num) => num * 2);
 console.log(doubledNumbers)
+
+/*
+The filter() function is used to create a new array with all 
+elements that pass the test implemented by the provided function.
+*/
+function customFilter(arr, callbackFn) {
+    const filteredArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if(callbackFn(arr[i], i, arr)) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
+
+const oddNumbers = customFilter(numbers, (num) => num % 2 != 0);
+console.log(oddNumbers);
